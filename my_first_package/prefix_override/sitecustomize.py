74c234c9edbb22ed6dd0c6b84cdbbb32f1a46ce3
Alejandro Bernal <75224058+albernalr@users.noreply.github.com>
2025-03-07 20:23:07 -0500

    Delete my_first_package directory

--- my_first_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/home/alejandro/.local/share/mamba/envs/ros_env':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alejandro/ros2_ws/phantom_ws/install/my_first_package'
